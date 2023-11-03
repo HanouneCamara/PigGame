dea184970ecdd3395e08958879ba08c96a4e99fe
HanouneCamara <Alicmr13@gmail.com>
2023-11-03 17:24:03 +0000

    playersNumber

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ def roll():
     
     return roll
 
-value = roll()
+while True:
+    players = input("Enter the number of players (2 - 4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2 - 4 players.")
+    else:
+        print("Invalid, try again.")
+        
+print(players)
+        
